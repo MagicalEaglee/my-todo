e18f370e2a458d90d10f94add3959827c38a62fa
Your Name <you@example.com>
2024-05-30 02:14:36 +0530

    change

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Jstar</h1>
+    <h1 className=' text-center text-5xl text-blue-400'>Jstar</h1>
     </>
   );
 }
