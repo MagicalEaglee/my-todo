6d95651d6e3e9ed6f24fbecd9e8768da5d83cd3d
Your Name <you@example.com>
2024-05-30 03:40:48 +0530

    to

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
   }
   return (
     <>
-    <h1 className='mt-2 mb-2 text-center text-5xl text-blue-400 font-extrabold'>Jstar Todo</h1>
+    <h1 className='mt-2 mb-2 text-center text-5xl text-blue-400 font-extrabold'>Time To Progress.....</h1>
     <div className='mx-auto w-2/4 border-blue-400 border-2 h-11 rounded-3xl overflow-auto'>
       <input className='outline-none w-3/4 px-5' value={value} onChange={handlechange} type="text"  placeholder="Write Something...."/>
       <button className='w-1/4 bg-blue-400 text-white h-full rounded-3xl' onClick={addTodo}>Add</button>
