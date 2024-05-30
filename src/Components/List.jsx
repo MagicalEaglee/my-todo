bd1a8db71ca3165cad5b9d2ca5e2d1aefa13db84
MagicalEaglee <seqtostore@gmail.com>
2024-05-30 14:30:52 +0530

    Update List.jsx

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -10,7 +10,7 @@ function List({todo,deleteI}) {
         todo.map((item,i)=>{
             return <li className=' list-none flex justify-between items-center px-3 mb-2 py-3 w-[100%] border-2 border-blue-400' key={i}>
                 <h2 className=' break-words w-3/4'>{item}</h2>
-                <button className='bg-black text-white px-4' onClick={()=>deleteI(i)}>X</button>
+                <button className='bg-black text-black px-4' onClick={()=>deleteI(i)}>X</button>
             </li>
         })
       }
